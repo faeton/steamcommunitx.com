a6dc22140d7dfc29f3033e94f6521bb7c809b26a
Faeton <faeton@gmail.com>
2018-10-07 22:43:53 +0300

    add id / customURL support

--- index.php
+++ index.php
@@ -4,14 +4,23 @@ require_once('vendor/autoload.php');
 $url = $_SERVER['REQUEST_URI'];
 list(,$type,$id) = explode('/', $url);
 
-if($type == 'profiles') {
-  // print $id;
+function durl($id) {
   $s = new SteamID( $id );
   $s3 = $s->RenderSteam3();
   preg_match('/\[U:\d:(\d+)\]/', $s3, $m);
   $steam = $m[1];
-  $durl = 'https://www.dotabuff.com/players/'.$steam;
-  header('Location: '.$durl);
+  return 'https://www.dotabuff.com/players/'.$steam;
+}
+
+function dUrlRedirect($id) {
+  header( 'Location: '.durl($id) );
+}
+
+if($type == 'profiles') {
+  dUrlRedirect($id);
+} elseif($type == 'id') {
+  $json = json_decode(file_get_contents('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=DB3924CFFCBD62DF56A4C109BC806985&vanityurl='.$id));
+  dUrlRedirect($json->response->steamid);
 } else {
-  die('Not supported url type: '. $type);
+  die('Not supported url type: '. $type. '. Please use full steamcommunity url.');
 }
